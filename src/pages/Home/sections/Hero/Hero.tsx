8b9207f9f9f6c5f6914f4450f52dff9b816ea5db
Isaque Santana Paixão <isaquesantanapaixao444@gmail.com>
2026-01-14 18:37:14 -0300

    fix: Hero component UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -11,20 +11,25 @@ const Hero = () => {
 
   const StyledHero = styled("div")(({ theme }) => ({
     backgroundColor: theme.palette.primary.main,
-    height: "100vh",
-    display: "flex",
-    alignItems: "center",
+    width: "100%",
     [theme.breakpoints.up('xs')]: {
-      paddingTop: "100px"
+      display: "block",
+      padding: "20px",
+      paddingTop: "100px",
+      paddingBottom: "40px",
 
     },
     [theme.breakpoints.up('md')]: {
-      paddingTop: "0px"
+      display: "flex",
+      alignItems: "center",
+      paddingTop: "100px",
+      height: "100vh"
     }
-}))
+  }))
 
   const StyledImg = styled("img")(({ theme }) => ({
-    width: "75%",
+    width: "80%",
+    position: "relative",
     borderRadius: "50%",
     border: `1px solid ${theme.palette.primary.contrastText}`
   }))
@@ -36,27 +41,27 @@ const Hero = () => {
           <Grid container spacing={2}>
             <Grid item xs={12} md={5}>
               <Box position="relative">
-                  <Box position="absolute" width={"150%"} top={-100} right={0}>
-                    <AnimatedBackground />
-                  </Box>
-                  <Box position="relative" textAlign="center">
-                    <StyledImg src={Avatar} />
-                  </Box>
+                <Box position="absolute" width={"150%"} top={-250} right={0}>
+                  <AnimatedBackground />
+                </Box>
+                <Box textAlign="center">
+                  <StyledImg src={Avatar} />
+                </Box>
               </Box>
-    
+
             </Grid>
             <Grid item xs={12} md={7}>
               <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Isaque Paixão</Typography>
               <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm fullstack</Typography>
               <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <StyledButton>
+                  <StyledButton onClick={() => console.log("Download")}>
                     <DownloadIcon />
                     <Typography>Download CV</Typography>
                   </StyledButton>
                 </Grid>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                  <StyledButton>
+                  <StyledButton onClick={() => console.log("Contact")}>
                     <MailOutlineIcon />
                     <Typography>Contact Me</Typography>
                   </StyledButton>
